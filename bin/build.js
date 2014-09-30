4e83d794858c1f6529e419dc44fb25666a93bcd8
FredyC <fredy.c@seznam.cz>
2014-09-30 15:20:21 +0200

    release 0.3.0 + browser bundle

--- bin/build.js
+++ bin/build.js
@@ -2,10 +2,19 @@
 
 var fs         = require('fs');
 var path       = require('path');
+var uglifyjs   = require('uglify-js');
+var browserify = require('browserify');
 var banner     = fs.readFileSync(__dirname + '/../LICENSE').toString()
-var src        = __dirname + '/../src';
-var lib        = __dirname + '/../lib';
-var pkg		   = require('../package.json');
+var src        = __dirname + '/../src/scent.coffee';
+var target     = __dirname + '/../lib/scent';
+var pkg        = require('../package.json');
+
+function minify(source) {
+  var opts = { fromString: true, mangle: {
+    toplevel: true
+  }};
+  return uglifyjs.minify(source, opts).code;
+}
 
 var bannerLines = banner.split("\n");
 for (var i = 0, ii = bannerLines.length; i < ii; i++) {
@@ -15,14 +24,42 @@ bannerLines.unshift("/*");
 bannerLines.push("* ", "* Version: "+pkg['version'], "*/", "");
 banner = bannerLines.join("\n");
 
-var coffee = require('coffee-script');
-function compile(file) {
-	var compiled = coffee.compile(fs.readFileSync(src + '/' + file).toString(), {
-  		bare: true
-	});
-	var out = lib + '/' + file.replace('.coffee', '.js')
-	fs.writeFileSync(out, banner + compiled);
-	console.log("written file "+out);
+var writeOut = function (suffix, content, cb) {
+  var fileName = path.resolve(target + suffix);
+  fs.writeFile(fileName, content, function(err) {
+    if (err) {
+      console.log("Error while writing to: ", fileName);
+      console.error(err.stack || err);
+    }
+    else {
+      console.log('Written file '+fileName);
+    }
+    cb && cb();
+  });
 }
 
-fs.readdirSync(src).forEach(compile);
+var coffee = require('coffee-script');
+var compiled = coffee.compile(fs.readFileSync(src).toString(), {
+  bare: true
+});
+writeOut('.js', banner + compiled, function() {
+
+  var bundleOptions = {
+    entries: target + '.js',
+    basedir: '../',
+    bundleExternal: false,
+    standalone: 'scent',
+  };
+
+  browserify(bundleOptions).bundle(function(err, buf) {
+    if (err) {
+      console.log("Error during bundling");
+      return console.error(err.stack || err);
+    }
+    var out = buf.toString();
+    writeOut('-browser.js', out);
+    writeOut('-browser.min.js', banner + minify(out));
+  });
+});
+
+writeOut('.min.js', banner + minify(compiled));
