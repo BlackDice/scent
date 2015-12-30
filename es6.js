1186cc8d0344348c499422bd431115ab239a84d9
FredyC <fredy.c@seznam.cz>
2015-12-30 22:07:17 +0100

    Create alias file for ES6 compatible sources
    Once we get rid of CoffeeScript, the location of these
    would change to src directory. Using this alias will help
    with transition later.

---
+++ es6.js
@@ -0,0 +1,10 @@
+export { Action } from './lib/es6/action';
+export { Component } from './lib/es6/component';
+export { Entity } from './lib/es6/entity';
+export { Engine } from './lib/es6/engine';
+export { Node } from './lib/es6/node';
+export { System } from './lib/es6/system';
+export { Symbols } from './lib/es6/symbols';
+
+import Scent from 'lib/es6/scent';
+export default Scent;
